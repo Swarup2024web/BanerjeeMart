76b27aedabe5a5d71b02758ef65ad05961283859
Swarup2024web <bswarup1998@gmail.com>
2025-08-04 10:04:20 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,45 +1,22 @@
-const CACHE_NAME = "swarup-academy-v1";
-
-const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/books.html",
-  "/notes.html",
-  "/manifest.json",
-  "/icon.png"
+const cacheName = 'swarup-cache-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-// Install event – cache files
-self.addEventListener("install", event => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
   );
-  self.skipWaiting();
 });
 
-// Activate event – clean up old caches
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keyList =>
-      Promise.all(
-        keyList.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
-  self.clients.claim();
-});
-
-// Fetch event – serve from cache if available
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response =>
-        response || fetch(event.request)
-      )
+    caches.match(event.request).then(res => res || fetch(event.request))
   );
 });
